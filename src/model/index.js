a8ed1ddea073f3bbeb62376514546555acacbdca
Maciej Sitko <maciej@neatcube.co.uk>
2016-05-15 19:01:34 +0100

    Description for statae container added

--- src/model/index.js
+++ src/model/index.js
@@ -2,7 +2,11 @@
 import Kefir from 'kefir';
 import def from './default';
 import test from './test';
-
+/*
+  Singleton State Container (Global Model)
+  ------------------------------
+  This particular model/state container combines the latest change, combines this stream and feeds it to the view.
+*/
 export default Kefir.combine([def,test], 
                ({counter},{name, data}) => 
                     (
