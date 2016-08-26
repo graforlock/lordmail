4f6beca154618692d580c688adc719e1d98ae756
Maciej Sitko <maciej@neatcube.co.uk>
2016-08-26 15:34:44 +0100

    Removed access origin (now) redundant in favour of sockets

--- server.js
+++ server.js
@@ -11,19 +11,12 @@ require('shelljs/global');
 
 app.use('/', express.static('./'));
 
-app.use('/*', function(req,res,next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-const server = app.listen('8080');
-const io = require('socket.io')(server);
+const server = app.listen('8080'),
+      io = require('socket.io')(server);
+
 io.on('connection', function(socket) {
     socket.on('build_template', function(layout) {
         fs.writeFileSync('test.html', layout, 'utf8');
