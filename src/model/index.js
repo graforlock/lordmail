4e09c51ce6c549e6e181680220cfb307f38dc047
Maciej Sitko <maciej@neatcube.co.uk>
2016-08-30 16:02:16 +0100

    Major state container refactor

--- src/model/index.js
+++ src/model/index.js
@@ -1,13 +1,51 @@
-
 import Kefir from 'kefir';
-import app from './app';
-import builder from './builder';
+import { emitState, pool } from '../utils/index';
+import actions from '../actions/index';
+import { builderProvider, stateProvider, appProvider } from './providers/index.js';
 
+const State = new stateProvider({
+    rows: [],
+    mode: {
+      trans: false,
+      menu: false,
+      weekly: false
+    },
+    templates: [],
+    launched: false,
+    prompt: ''
+});
 
-const appState = Kefir.combine([app, builder], 
-                    ({launched, prompt}, builder) => 
-                      ({ launched,
-                         prompt,
-                         builder }));
-                
-export default appState;
+State.subscribe(function(state) {
+  let state$ = emitState(state);
+  model.plug(state$);
+})
+
+const model = Kefir.pool(),
+      Builder = builderProvider.getInstance(State),
+      App = appProvider.getInstance(State);
+
+//-> Web sockets listeners :
+Builder.onTemplateList();
+Builder.onChangedTemplate();
+
+pool.onValue(x => {
+  switch (x.type) {
+    case actions.LAUNCH_CREATOR:
+        App.launchCreator();
+        break;
+    case actions.GET_PROMPT:
+        App.getPromptvalue(x.payload);
+        break;
+    case actions.RENDER_TEMPLATE:
+        Builder.renderTemplate(x.payload);
+        break;
+    case actions.SEND_EMAIL:
+        Builder.renderTemplate(x.payload);
+        break;
+    case actions.UPDATE_SCHEMA:
+        Builder.updateSchema(x.payload);
+        break;
+  }
+});
+
+export default model;
--- src/views/root.js
+++ src/views/root.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Launch from './launch';
 import Builder from './builder';
 
-const Root = ({launched, prompt, builder: {rows, mode, templates}}) => {
+const Root = ({launched, prompt, rows, mode, templates}) => {
     let overlayClass = launched || prompt ? 'main-overlay hide' : 'main-overlay',
         builderShow = launched  || prompt ? true : false;
     return (
